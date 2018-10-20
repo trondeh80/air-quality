f643a3741039112226068e8fd91e2148fe190275
trondeh80 <trond.erling@gmail.com>
2018-10-20 15:49:01 +0200

    Initial commit

---
+++ src/js/reducers/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+
+import {
+    CHOOSE_CITY,
+    FETCH_READINGS,
+    FETCH_READINGS_RESOLVED
+} from '../actions'
+
+export default function rootReducer() {
+
+}
